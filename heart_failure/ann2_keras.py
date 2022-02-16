d33ab9369db3db64bfc23b0eec6deffa61382b18
gt <jack139@gmail.com>
2022-02-16 15:26:47 +0800

    add another ann

---
+++ heart_failure/ann2_keras.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+#import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+#import seaborn as sns
+from keras.layers import Dense, BatchNormalization, Dropout, LSTM
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras import callbacks
+from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score
+
+#loading data
+data = pd.read_csv("../datasets/heart_failure_clinical_records_dataset.csv")
+data.head()
+
+data.info()
+
+data.describe().T
+
+#assigning values to features as X and target as y
+X=data.drop(["DEATH_EVENT"],axis=1)
+y=data["DEATH_EVENT"]
+
+#Set up a standard scaler for the features
+col_names = list(X.columns)
+s_scaler = preprocessing.StandardScaler()
+X_df= s_scaler.fit_transform(X)
+X_df = pd.DataFrame(X_df, columns=col_names)   
+X_df.describe().T
+
+#spliting test and training sets
+X_train, X_test, y_train,y_test = train_test_split(X_df,y,test_size=0.25,random_state=7)
+
+
+#MODEL BUILDING
+
+early_stopping = callbacks.EarlyStopping(
+    min_delta=0.001, # minimium amount of change to count as an improvement
+    patience=20, # how many epochs to wait before stopping
+    restore_best_weights=True)
+
+# Initialising the NN
+model = Sequential()
+
+# layers
+model.add(Dense(units = 16, kernel_initializer = 'uniform', activation = 'relu', input_dim = 12))
+model.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dropout(0.25))
+model.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+from keras.optimizers import SGD
+# Compiling the ANN
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Train the ANN
+history = model.fit(X_train, y_train, batch_size = 32, epochs = 500,callbacks=[early_stopping], validation_split=0.2)
+
+# test result
+val_accuracy = np.mean(history.history['val_accuracy'])
+print("\n%s: %.2f%%" % ('val_accuracy', val_accuracy*100))
+
+
+# Predicting the test set results
+y_pred = model.predict(X_test)
+y_pred = (y_pred > 0.5)
+np.set_printoptions()
+
+
+print(classification_report(y_test, y_pred))
